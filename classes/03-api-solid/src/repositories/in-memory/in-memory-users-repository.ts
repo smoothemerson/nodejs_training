501f1f6b8f0c4678cc8694fc8da2dd7dfea464c8
Emerson <emerson.faria@serpro.gov.br>
2025-02-28 12:58:44 -0300

    check-in use case

--- classes/03-api-solid/src/repositories/in-memory/in-memory-users-repository.ts
+++ classes/03-api-solid/src/repositories/in-memory/in-memory-users-repository.ts
@@ -1,5 +1,6 @@
 import type { Prisma, User } from '@prisma/client'
 import type { UsersRepository } from '../users-repository'
+import { randomUUID } from 'node:crypto'
 
 export class InMemoryUsersRepository implements UsersRepository {
   public items: User[] = []
@@ -26,7 +27,7 @@ export class InMemoryUsersRepository implements UsersRepository {
 
   async create(data: Prisma.UserCreateInput) {
     const user = {
-      id: 'user-1',
+      id: randomUUID(),
       name: data.name,
       email: data.email,
       password_hash: data.password_hash,
@@ -38,13 +39,3 @@ export class InMemoryUsersRepository implements UsersRepository {
     return user
   }
 }
-
-// {
-//   async findByEmail() {
-//     return null
-//   },
-
-//   async create(data) {
-//
-//   },
-// }
