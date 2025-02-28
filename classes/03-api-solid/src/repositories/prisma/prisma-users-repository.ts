c629e397e4ad74289db52699b24a23f81e58ec07
Emerson <emerson.faria@serpro.gov.br>
2025-02-28 12:44:22 -0300

    profile use case

--- classes/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
+++ classes/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
@@ -1,5 +1,5 @@
 import { prisma } from '@/lib/prisma'
-import type { Prisma } from '@prisma/client'
+import type { Prisma, User } from '@prisma/client'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
