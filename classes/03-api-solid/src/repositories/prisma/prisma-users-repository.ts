62fca8381472da2ceed529386f6eff99045fb8ac
emerson <emerson.faria@serpro.gov.br>
2025-03-07 11:58:15 -0300

    prisma repositories

--- classes/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
+++ classes/03-api-solid/src/repositories/prisma/prisma-users-repository.ts
@@ -1,8 +1,18 @@
 import { prisma } from '@/lib/prisma'
-import type { Prisma, User } from '@prisma/client'
+import type { Prisma } from '@prisma/client'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
