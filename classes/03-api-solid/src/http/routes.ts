ab99f7147cb16eef3bfcd6a0bd5b3f58b3e94d2f
emerson <emerson8096@gmail.com>
2025-03-10 11:30:34 -0300

    JWT authentication principles

--- classes/03-api-solid/src/http/routes.ts
+++ classes/03-api-solid/src/http/routes.ts
@@ -1,9 +1,11 @@
 import type { FastifyInstance } from 'fastify'
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
-
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
