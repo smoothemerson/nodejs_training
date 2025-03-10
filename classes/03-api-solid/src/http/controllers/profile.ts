a329a235dab770e2417aa243b2a2f0bfad7b3198
emerson <emerson8096@gmail.com>
2025-03-10 12:07:52 -0300

    implementing JWT in fastify

--- classes/03-api-solid/src/http/controllers/profile.ts
+++ classes/03-api-solid/src/http/controllers/profile.ts
@@ -1,6 +1,7 @@
 import type { FastifyRequest, FastifyReply } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  
+  await request.jwtVerify()
+
   return reply.status(200).send()
 }
