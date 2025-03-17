16891bdc147105638847718ce8987ac14a5663e6
emerson <emerson8096@gmail.com>
2025-03-17 10:22:47 -0300

    refresh tokenm implementation

--- classes/03-api-solid/src/http/controllers/users/routes.ts
+++ classes/03-api-solid/src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { register } from './register'
 import { verifyJWT } from '@/http/middlewares/verify-jwt'
 import { authenticate } from './authenticate'
 import { profile } from './profile'
+import { refresh } from './refresh'
 
 export async function usersRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.patch('/token/refresh', refresh)
 
   app.get(
     '/me',
