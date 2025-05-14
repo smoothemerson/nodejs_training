b878096a652324df0bf67f8afcec7fc30bea0c25
emerson <emerson8096@gmail.com>
2025-05-14 07:17:17 -0300

    feat: add authenticate user controller

---
+++ challenges/find_a_friend/src/http/controllers/users/authenticate-user.ts
@@ -0,0 +1,34 @@
+import { InvalidCredentialsError } from '@/use-cases/errors/invalid-credentials-error'
+import { makeAuthenticateUserUseCase } from '@/use-cases/factories/make-authenticate-user-use-case'
+import type { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function authenticateUser(
+  request: FastifyRequest,
+  reply: FastifyReply,
+) {
+  const authenticateUserBodySchema = z.object({
+    email: z.string(),
+    password: z.string().min(6),
+  })
+
+  const { email, password } = authenticateUserBodySchema.parse(request.body)
+
+  try {
+    const authenticateUserUseCase = makeAuthenticateUserUseCase()
+
+    const { user } = await authenticateUserUseCase.execute({ email, password })
+
+    const token = await reply.jwtSign({}, { sign: { sub: user.id } })
+
+    return reply.status(200).send({
+      token,
+    })
+  } catch (err) {
+    if (err instanceof InvalidCredentialsError) {
+      return reply.status(400).send({ message: err.message })
+    }
+
+    throw err
+  }
+}
