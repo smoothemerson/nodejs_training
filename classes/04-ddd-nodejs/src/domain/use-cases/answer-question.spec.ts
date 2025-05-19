770161fbdff4bf9232617ea3c8328aef05494795
emerson <emerson8096@gmail.com>
2025-05-19 14:35:17 -0300

    external dependencies

--- classes/04-ddd-nodejs/src/domain/use-cases/answer-question.spec.ts
+++ classes/04-ddd-nodejs/src/domain/use-cases/answer-question.spec.ts
@@ -1,10 +1,18 @@
 import { expect, test } from 'vitest'
+import type { Answer } from '../entities/answer'
+import type { AnswersRepository } from '../repositories/answers-repository'
 import { AnswerQuestionUseCase } from './answer-question'
 
-test('create an answer', () => {
-  const answerQuestion = new AnswerQuestionUseCase()
+const fakeAnswerRepository: AnswersRepository = {
+  create: async (answer: Answer) => {
+    return
+  },
+}
 
-  const answer = answerQuestion.execute({
+test('create an answer', async () => {
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
+
+  const answer = await answerQuestion.execute({
     questionId: '1',
     instructorId: '1',
     content: 'Nova resposta',
