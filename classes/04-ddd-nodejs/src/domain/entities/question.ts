78ac91792e173b6cd23c0fdfa77fbee2713df7e2
emerson <emerson8096@gmail.com>
2025-05-20 10:46:42 -0300

    mapping properties

--- classes/04-ddd-nodejs/src/domain/entities/question.ts
+++ classes/04-ddd-nodejs/src/domain/entities/question.ts
@@ -1,11 +1,15 @@
 import { Entity } from '../../core/entities/entity'
+import type { UniqueEntityID } from '../../core/entities/unique-entity-id'
 import type { Slug } from './value-objects/slug'
 
 interface QuestionProps {
+  authorId: UniqueEntityID
+  bestAnswerId?: UniqueEntityID
   title: string
   content: string
   slug: Slug
-  authorId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
 export class Question extends Entity<QuestionProps> {}
