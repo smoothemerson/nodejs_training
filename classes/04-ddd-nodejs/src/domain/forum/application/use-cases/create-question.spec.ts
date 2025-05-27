f3f84e45b265a5f23472683e01d88e33428d318a
emerson <emerson8096@gmail.com>
2025-05-27 11:27:10 -0300

    use case: create question

---
+++ classes/04-ddd-nodejs/src/domain/forum/application/use-cases/create-question.spec.ts
@@ -0,0 +1,21 @@
+import type { Question } from '../../enterprise/entities/question'
+import type { QuestionsRepository } from '../repositories/questions-repository'
+import { CreateQuestionUseCase } from './create-question'
+
+const fakeQuestionsRepository: QuestionsRepository = {
+  create: async (question: Question) => {
+    console.log(question)
+  },
+}
+
+test('create a question', async () => {
+  const createQuestion = new CreateQuestionUseCase(fakeQuestionsRepository)
+
+  const { question } = await createQuestion.execute({
+    authorId: '1',
+    title: 'Nova pergunta',
+    content: 'Conteúdo da pergunta',
+  })
+
+  expect(question.id).toBeTruthy()
+})
