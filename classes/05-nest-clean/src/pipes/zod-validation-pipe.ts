7f685a98fc9aa2797a084d10a1f13b4344e1d02b
emerson <emerson8096@gmail.com>
2025-07-21 08:53:15 -0300

    creating zod validation pipe

---
+++ classes/05-nest-clean/src/pipes/zod-validation-pipe.ts
@@ -0,0 +1,23 @@
+import { BadRequestException, PipeTransform } from '@nestjs/common'
+import { z, ZodError } from 'zod'
+
+export class ZodValidationPipe implements PipeTransform {
+  constructor(private schema: z.ZodType) {}
+
+  transform(value: unknown) {
+    try {
+      this.schema.parse(value)
+    } catch (error) {
+      if (error instanceof ZodError) {
+        throw new BadRequestException({
+          errors: z.treeifyError(error),
+          message: 'Validation failed',
+          statusCode: 400,
+        })
+      }
+
+      throw new BadRequestException('Validation failed')
+    }
+    return value
+  }
+}
