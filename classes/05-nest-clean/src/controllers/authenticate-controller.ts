6f50b3c9ce6213712d3fa67ec90fc1622641d1e8
emerson <emerson8096@gmail.com>
2025-07-24 07:16:23 -0300

    generating jwt token

---
+++ classes/05-nest-clean/src/controllers/authenticate-controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Post } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(private jwt: JwtService) {}
+
+  @Post()
+  async handle() {
+    const token = await this.jwt.sign({
+      sub: 'user-id',
+    })
+
+    return token
+  }
+}
