88296153eea053e073e603e999267dbcd82662cd
emerson <emerson8096@gmail.com>
2025-08-05 07:48:58 -0300

    implementing prisma repositories

--- classes/05-nest-clean/src/infra/http/controllers/authenticate.controller.e2e-spec.ts
+++ classes/05-nest-clean/src/infra/http/controllers/authenticate.controller.e2e-spec.ts
@@ -3,7 +3,7 @@ import { Test } from '@nestjs/testing'
 import { hash } from 'bcryptjs'
 import request from 'supertest'
 import { AppModule } from '@/infra/app.module'
-import { PrismaService } from '@/infra/prisma/prisma.service'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 describe('Authenticate (E2E)', () => {
   let app: INestApplication
