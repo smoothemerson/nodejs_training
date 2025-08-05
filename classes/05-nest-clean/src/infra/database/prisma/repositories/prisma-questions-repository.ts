87d93d9ec2c7d70128734ba1f3a145b1e25f0b45
emerson <emerson8096@gmail.com>
2025-08-05 08:03:17 -0300

    conversation between layers (mappers)

--- classes/05-nest-clean/src/infra/database/prisma/repositories/prisma-questions-repository.ts
+++ classes/05-nest-clean/src/infra/database/prisma/repositories/prisma-questions-repository.ts
@@ -2,11 +2,23 @@ import { Injectable } from '@nestjs/common'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
 import { Question } from '@/domain/forum/enterprise/entities/question'
+import { PrismaQuestionMapper } from '../mappers/prisma-question-mapper'
+import { PrismaService } from '../prisma.service'
 
 @Injectable()
 export class PrismaQuestionsRepository implements QuestionsRepository {
-  findById(id: string): Promise<Question | null> {
-    throw new Error('Method not implemented.')
+  constructor(private prisma: PrismaService) {}
+
+  async findById(id: string) {
+    const question = await this.prisma.question.findUnique({
+      where: { id },
+    })
+
+    if (!question) {
+      return null
+    }
+
+    return PrismaQuestionMapper.toDomain(question)
   }
 
   findBySlug(slug: string): Promise<Question | null> {
