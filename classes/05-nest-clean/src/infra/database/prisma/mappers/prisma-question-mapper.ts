0371f41388b46e06f5b7490626d1edf98359998f
emerson <emerson8096@gmail.com>
2025-08-06 07:55:00 -0300

    implementing questions repository

--- classes/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ classes/05-nest-clean/src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,4 +1,4 @@
-import { Question as PrismaQuestion } from '@/../generated/prisma'
+import { Prisma, Question as PrismaQuestion } from '@/../generated/prisma'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Question } from '@/domain/forum/enterprise/entities/question'
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug'
@@ -19,5 +19,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id)
     )
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    }
   }
 }
