9a21c876dae42ca6009b22feb32c7b6571aa7229
emerson <emerson8096@gmail.com>
2025-08-08 07:56:07 -0300

    questions presenter

--- classes/05-nest-clean/src/infra/http/controllers/fetch-recent-questions.controller.ts
+++ classes/05-nest-clean/src/infra/http/controllers/fetch-recent-questions.controller.ts
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard'
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe'
+import { QuestionPresenter } from '../presenters/question-presenter'
 
 const pageQueryParamSchema = z
   .string()
@@ -22,12 +23,18 @@ export class FetchRecentQuestionsController {
 
   @Get()
   async handle(@Query('page', queryValidationPipe) page: PageQueryParamSchema) {
-    const questions = await this.fetchRecentQuestions.execute({
+    const result = await this.fetchRecentQuestions.execute({
       page,
     })
 
+    if (result.ifLeft()) {
+      throw new Error()
+    }
+
+    const questions = result.value.questions
+
     return {
-      questions,
+      questions: questions.map(QuestionPresenter.toHttp),
     }
   }
 }
