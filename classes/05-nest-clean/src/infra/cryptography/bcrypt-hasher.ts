8be8629e8fd4eb0594e25bb6ff22cddcbe193c94
emerson <emerson8096@gmail.com>
2025-08-12 07:47:18 -0300

    cryptography implementation

---
+++ classes/05-nest-clean/src/infra/cryptography/bcrypt-hasher.ts
@@ -0,0 +1,15 @@
+import { compare, hash } from 'bcryptjs'
+import { HashComparer } from '@/domain/forum/application/cryptography/hash-comparer'
+import { HashGenerator } from '@/domain/forum/application/cryptography/hash-generator'
+
+export class BcryptHasher implements HashGenerator, HashComparer {
+  private HASH_SALT_LENGTH = 8
+
+  hash(plain: string) {
+    return hash(plain, this.HASH_SALT_LENGTH)
+  }
+
+  compare(plain: string, hash: string) {
+    return compare(plain, hash)
+  }
+}
