49986a07c1708ef04837c78b9844ca4be89afdf7
emerson <emerson8096@gmail.com>
2025-08-19 08:15:02 -0300

    controller: get question by slug

--- classes/05-nest-clean/src/domain/forum/application/use-cases/get-question-by-slug.ts
+++ classes/05-nest-clean/src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -1,7 +1,8 @@
+import { Injectable } from '@nestjs/common'
 import { Either, left, right } from '@/core/either'
-import { Question } from '../../enterprise/entities/question'
-import type { QuestionsRepository } from '../repositories/questions-repository'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { Question } from '../../enterprise/entities/question'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface GetQuestionBySlugUseCaseRequest {
   slug: string
@@ -14,6 +15,7 @@ type GetQuestionBySlugUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class GetQuestionBySlugUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
