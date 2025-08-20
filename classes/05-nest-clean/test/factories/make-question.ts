01c1ea03c46cff7bd8feeedc956cfe37b861a426
emerson <emerson8096@gmail.com>
2025-08-20 07:42:59 -0300

    using factories in e2e tests

--- classes/05-nest-clean/test/factories/make-question.ts
+++ classes/05-nest-clean/test/factories/make-question.ts
@@ -1,13 +1,16 @@
 import { faker } from '@faker-js/faker'
+import { Injectable } from '@nestjs/common'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { PrismaQuestionMapper } from '@/infra/database/prisma/mappers/prisma-question-mapper'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function makeQuestion(
   override: Partial<QuestionProps> = {},
-  id?: UniqueEntityID,
+  id?: UniqueEntityID
 ) {
   const question = Question.create(
     {
@@ -16,8 +19,25 @@ export function makeQuestion(
       content: faker.lorem.text(),
       ...override,
     },
-    id,
+    id
   )
 
   return question
+}
+
+@Injectable()
+export class QuestionFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestion(
+    data: Partial<QuestionProps> = {}
+  ): Promise<Question> {
+    const question = makeQuestion(data)
+
+    await this.prisma.question.create({
+      data: PrismaQuestionMapper.toPrisma(question),
+    })
+
+    return question
+  }
 }
