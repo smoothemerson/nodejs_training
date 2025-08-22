a7e7747b2b44f57a97edaf65a2ae5c25e0093e7b
emerson <emerson8096@gmail.com>
2025-08-22 07:37:02 -0300

    controller: delete question

--- classes/05-nest-clean/src/domain/forum/application/use-cases/delete-question.ts
+++ classes/05-nest-clean/src/domain/forum/application/use-cases/delete-question.ts
@@ -1,7 +1,8 @@
+import { Injectable } from '@nestjs/common'
 import { Either, left, right } from '@/core/either'
 import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
-import type { QuestionsRepository } from '../repositories/questions-repository'
+import { QuestionsRepository } from '../repositories/questions-repository'
 
 interface DeleteQuestionUseCaseRequest {
   authorId: string
@@ -13,6 +14,7 @@ type DeleteQuestionUseCaseResponse = Either<
   null
 >
 
+@Injectable()
 export class DeleteQuestionUseCase {
   constructor(private questionsRepository: QuestionsRepository) {}
 
