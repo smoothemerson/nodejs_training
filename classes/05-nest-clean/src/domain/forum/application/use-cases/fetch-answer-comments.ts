7c0ef97efd9fcd597502a8b3694f2d1752b57495
emerson <emerson8096@gmail.com>
2025-09-01 07:29:52 -0300

    controller: fetch answer comments

--- classes/05-nest-clean/src/domain/forum/application/use-cases/fetch-answer-comments.ts
+++ classes/05-nest-clean/src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -1,3 +1,4 @@
+import { Injectable } from '@nestjs/common'
 import { Either, right } from '@/core/either'
 import { AnswerComment } from '../../enterprise/entities/answer-comment'
 import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
@@ -14,6 +15,7 @@ type FetchAnswerCommentsUseCaseResponse = Either<
   }
 >
 
+@Injectable()
 export class FetchAnswerCommentsUseCase {
   constructor(private answerCommentsRepository: AnswerCommentsRepository) {}
 
