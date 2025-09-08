dcf91a330cb8a408674b9cdbde5b0974ce62d6d7
emerson <emerson8096@gmail.com>
2025-09-08 08:02:07 -0300

    editing questions with attachments

--- classes/05-nest-clean/test/factories/make-question-attachment.ts
+++ classes/05-nest-clean/test/factories/make-question-attachment.ts
@@ -1,12 +1,14 @@
+import { Injectable } from '@nestjs/common'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import {
   QuestionAttachment,
   QuestionAttachmentProps,
 } from '@/domain/forum/enterprise/entities/question-attachment'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
 
 export function makeQuestionAttachment(
   override: Partial<QuestionAttachmentProps> = {},
-  id?: UniqueEntityID,
+  id?: UniqueEntityID
 ) {
   const questionAttachment = QuestionAttachment.create(
     {
@@ -14,8 +16,28 @@ export function makeQuestionAttachment(
       attachmentId: new UniqueEntityID(),
       ...override,
     },
-    id,
+    id
   )
 
   return questionAttachment
+}
+
+@Injectable()
+export class QuestionAttachmentFactory {
+  constructor(private prisma: PrismaService) {}
+
+  async makePrismaQuestionAttachment(
+    data: Partial<QuestionAttachmentProps> = {}
+  ): Promise<QuestionAttachment> {
+    const questionAttachment = makeQuestionAttachment(data)
+
+    await this.prisma.attachment.update({
+      where: { id: questionAttachment.attachmentId.toString() },
+      data: {
+        questionId: questionAttachment.questionId.toString(),
+      },
+    })
+
+    return questionAttachment
+  }
 }
