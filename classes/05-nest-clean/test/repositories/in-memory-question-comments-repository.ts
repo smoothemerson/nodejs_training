143674d52f44c4f01546de52ab07d70d29c53139
emerson <emerson8096@gmail.com>
2025-09-10 07:51:28 -0300

    fetching comments with author

--- classes/05-nest-clean/test/repositories/in-memory-question-comments-repository.ts
+++ classes/05-nest-clean/test/repositories/in-memory-question-comments-repository.ts
@@ -1,14 +1,18 @@
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
 import { QuestionComment } from '@/domain/forum/enterprise/entities/question-comment'
+import { CommentWithAuthor } from '@/domain/forum/enterprise/entities/value-objects/comment-with-author'
+import { InMemoryStudentsRepository } from './in-memory-students-repository'
 
 export class InMemoryQuestionCommentsRepository
   implements QuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  constructor(private studentsRepository: InMemoryStudentsRepository) {}
+
   async findById(id: string) {
-    const questionComment = this.items.find((item) => item.id.toString() === id)
+    const questionComment = this.items.find(item => item.id.toString() === id)
 
     if (!questionComment) {
       return null
@@ -19,8 +23,39 @@ export class InMemoryQuestionCommentsRepository
 
   async findManyByQuestionId(questionId: string, { page }: PaginationParams) {
     const questionComments = this.items
-      .filter((item) => item.questionId.toString() === questionId)
+      .filter(item => item.questionId.toString() === questionId)
       .slice((page - 1) * 20, page * 20)
+
+    return questionComments
+  }
+
+  async findManyByQuestionIdWithAuthor(
+    questionId: string,
+    { page }: PaginationParams
+  ) {
+    const questionComments = this.items
+      .filter(item => item.questionId.toString() === questionId)
+      .slice((page - 1) * 20, page * 20)
+      .map(comment => {
+        const author = this.studentsRepository.items.find(student => {
+          return student.id.equals(comment.authorId)
+        })
+
+        if (!author) {
+          throw new Error(
+            `Author with ID "${comment.authorId.toString()}" does not exists.`
+          )
+        }
+
+        return CommentWithAuthor.create({
+          commentId: comment.id,
+          content: comment.content,
+          authorId: comment.authorId,
+          author: author.name,
+          createdAt: comment.createdAt,
+          updatedAt: comment.updatedAt,
+        })
+      })
 
     return questionComments
   }
@@ -31,7 +66,7 @@ export class InMemoryQuestionCommentsRepository
 
   async delete(questionComment: QuestionComment) {
     const itemIndex = this.items.findIndex(
-      (item) => item.id === questionComment.id,
+      item => item.id === questionComment.id
     )
 
     this.items.splice(itemIndex, 1)
